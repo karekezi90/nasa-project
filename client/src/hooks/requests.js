d2d3e63fc2ff8574eb93c498ee4eb68346ca40aa
karekezi90 <28229829+karekezi90@users.noreply.github.com>
2022-02-02 15:28:50 +0800

    initital

---
+++ client/src/hooks/requests.js
@@ -0,0 +1,54 @@
+const API_URL = 'http://localhost:8000/v1'
+
+// Load planets and return as JSON.
+async function httpGetPlanets() {
+  const response = await fetch(`${API_URL}/planets`)
+  return await response.json()
+}
+
+// Load launches, sort by flight number, and return as JSON.
+async function httpGetLaunches() {
+  const response = await fetch(`${API_URL}/launches`)
+  const fetchedLauches = await response.json()
+  return fetchedLauches.sort((a, b) => {
+    return a.flightNumber - b.flightNumber
+  })
+}
+
+// Submit given launch data to launch system.
+async function httpSubmitLaunch(launch) {
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(launch)
+    })
+  } catch (error) {
+    return {
+      ok: false
+    }
+  }
+}
+
+// Delete launch with given ID.
+async function httpAbortLaunch(id) {
+  try {
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: 'DELETE'
+    })
+  } catch (error) {
+    console.log(error)
+    return {
+      ok: false
+    }
+  }
+}
+
+export {
+  httpGetPlanets,
+  httpGetLaunches,
+  httpSubmitLaunch,
+  httpAbortLaunch,
+};
